2b263f08697a9e8574878c995069b111a195cb36
Peter Vandiver <pjvandiv@gmail.com>
2015-03-18 13:45:13 -0400

    stuff

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,7 @@
 
 # Example:
 #
-set :output, "/Users/petervandiver/Desktop/programs/Tasket_application/cron_log.log"
+# set :output, "/Users/petervandiver/Desktop/programs/Tasket_application/cron_log.log"
 #
 # every 2.hours do
 #   command "/usr/bin/some_great_command"
